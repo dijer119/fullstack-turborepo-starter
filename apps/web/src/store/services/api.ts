6a27b699fc1807fc8117a77beba451a2f7698290
dijer <dijer119@gmail.com>
2025-11-30 07:13:02 +0900

    web -> api

--- apps/web/src/store/services/api.ts
+++ apps/web/src/store/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "api",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
   }),
   tagTypes: [],
   endpoints: (builder) => ({
